b88148f955449c5614910687cb5b307de9a5b201
Demiurg20 <islamasylbekov1012@gmail.com>
2023-12-09 22:27:28 +0000

    yesss

--- app/src/main/java/com/example/xdd2/FourthActivity.kt
+++ app/src/main/java/com/example/xdd2/FourthActivity.kt
@@ -1,11 +1,18 @@
 package com.example.xdd2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
+    }
+
+    fun onClickGoMain(view : View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
